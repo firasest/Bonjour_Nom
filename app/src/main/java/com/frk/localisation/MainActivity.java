0632e1bcea47fbf0d5a321483d6e89fd2627cc14
firasest <jlassifiras6@gmail.com>
2019-10-02 12:40:24 +0100

    commit final

--- app/src/main/java/com/frk/localisation/MainActivity.java
+++ app/src/main/java/com/frk/localisation/MainActivity.java
@@ -3,9 +3,18 @@ package com.frk.localisation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    TextView textView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,13 +22,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-        textView.findviewById(R.id.textViewNom);
+        textView.findViewById(R.id.TextViewBonjour);
         editText.findViewById(R.id.EditTextNom);
         button.findViewById(R.id.ButtonEnvoyer);
+        button.setOnClickListener(new View.OnClickListener() {
 
-        button.setOnclickListener(new View.OnClickListener(){
 
-            drt
-        });
+        @Override
+        public void onClick(View v) {
+            String message = getResources().getString(R.string.message) + "" + editText.getText() + ",";
+            textView.setText(message);
+
+        }
+    });
+
     }
 }
